c7470a4f33a9ed5250467ac63bfc2e6fc5e83685
julia953197 <julia953197@yandex.ru>
2022-06-02 14:40:58 +0300

    Fix names in header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,11 +5,11 @@ import logoPath from "../images/logo.svg"
 function Header(){
     return (
         <header className="header">
-            <a href="" className="header__link">About</a>
             <a href="" className="header__link">Explore</a>
-            <img src={logoPath} className="header__logo"/>
             <a href="" className="header__link">Create</a>
+            <img src={logoPath} className="header__logo"/>
             <a href="" className="header__link">Menu</a>
+            <a href="" className="header__link">Signout</a>
         </header>
     )
 }
