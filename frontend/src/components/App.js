a3de89afc0d1505efdb8521cb46c928d55668536
julia953197 <julia953197@yandex.ru>
2022-06-03 14:22:10 +0300

    Closed #6

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import About from "./About";
+
 function App(){
     return (
         <div className="App">
             <Header/>
+            <About/>
         </div>
     )
 }
